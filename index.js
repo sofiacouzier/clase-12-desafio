0d3265b5628e126c86a30b5896d4c13f06e0aa67
Sofia Couzier <soficouzier@gmail.com>
2022-12-06 22:53:12 -0300

    desafio

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+let app = express();
+const PORT = 3001;
+let path = require("path");
+let { Server: HttpServer } = require("http");
+
+let serverRoutes = require("./routes");
+let Socket = require("./utils/sockets");
+
+
+//midlewares
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("./public"));
+
+// view engine
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+serverRoutes(app)
+
+
+
+let httpServer = new HttpServer(app);
+
+
+let socket = new Socket(httpServer);
+
+socket.init();
+
+httpServer.listen(PORT, () => console.log(`http://localhost:${PORT}`));
